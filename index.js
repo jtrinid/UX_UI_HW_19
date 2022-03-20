0583b023c34c503a58368f61aae50aec299a5e57
jtrinid <91659230+jtrinid@users.noreply.github.com>
2022-03-20 16:59:02 -0400

    jquery updates

---
+++ index.js
@@ -0,0 +1,16 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+const menuBtn = document.querySelector(".containerFilter");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+    if(!menuOpen) {
+        menuBtn.classList.add("open");
+        $(".accordionContainer").show();
+    } else {
+        menuBtn.classList.remove("open");
+        menuOpen = false;
+        $(".accordionContainer").hide();
+    }
+});
