42f8d4167dca1bde6b42fb3ffe7e70c24f8dee1e
jtrinid <91659230+jtrinid@users.noreply.github.com>
2022-03-20 17:40:43 -0400

    peer feedback iteration
    Incorporating peer review and feedback on jquery for function and visual display (Heather and Phil)

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let menuOpen = false;
 menuBtn.addEventListener("click", () => {
     if(!menuOpen) {
         menuBtn.classList.add("open");
+        menuOpen = true;
         $(".accordionContainer").show();
     } else {
         menuBtn.classList.remove("open");
